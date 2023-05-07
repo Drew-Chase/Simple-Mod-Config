2c387fb7e8795a7f330aec178b51b5568e3efba5
Drew Chase <dcmanproductions@gmail.com>
2023-05-07 17:34:30 -0400

    added logger

--- common/src/main/java/chase/minecraft/architectury/simplemodconfig/SimpleModConfig.java
+++ common/src/main/java/chase/minecraft/architectury/simplemodconfig/SimpleModConfig.java
@@ -1,10 +1,15 @@
 package chase.minecraft.architectury.simplemodconfig;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 public class SimpleModConfig
 {
 	public static final String MOD_ID = "simple-mod-config";
-
-	public static void init() {
-		
+	public static final Logger log = LogManager.getLogger(MOD_ID);
+	
+	public static void init()
+	{
+		log.info("Initializing Simple Mod Config");
 	}
 }
