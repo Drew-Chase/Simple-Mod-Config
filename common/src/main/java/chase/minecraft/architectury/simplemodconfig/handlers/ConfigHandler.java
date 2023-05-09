f809d81c47186cdbe89b221e38546e1893a60a2c
Drew Chase <dcmanproductions@gmail.com>
2023-05-09 00:00:53 -0400

    added tooltip, sorted list, reset function, etc

--- common/src/main/java/chase/minecraft/architectury/simplemodconfig/handlers/ConfigHandler.java
+++ common/src/main/java/chase/minecraft/architectury/simplemodconfig/handlers/ConfigHandler.java
@@ -1,9 +1,13 @@
 package chase.minecraft.architectury.simplemodconfig.handlers;
 
 import chase.minecraft.architectury.simplemodconfig.SimpleModConfig;
+import chase.minecraft.architectury.simplemodconfig.annotation.SimpleConfig;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import dev.architectury.platform.Platform;
+import net.minecraft.ChatFormatting;
+import net.minecraft.network.chat.Component;
+import net.minecraft.network.chat.MutableComponent;
 import org.jetbrains.annotations.Nullable;
 
 import java.io.File;
@@ -11,9 +15,10 @@ import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.lang.reflect.Field;
-import java.lang.reflect.Type;
 import java.nio.file.Path;
 import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Objects;
 
 /**
  * The ConfigHandler class is a generic class that handles loading, saving, setting, and getting configuration data from a JSON file using the Gson library.
@@ -21,9 +26,11 @@ import java.util.HashMap;
 public class ConfigHandler<T>
 {
 	public final File CONFIG_FILE;
+	private final String name;
+	
+	private Component displayName;
 	private T config;
 	private final T initialConfig;
-	private final HashMap<String, Type> fields;
 	
 	/**
 	 * Creates a new ConfigHandler object
@@ -31,11 +38,12 @@ public class ConfigHandler<T>
 	 * @param name          The name of the json file not including the .json
 	 * @param initialConfig The config's initial values.
 	 */
-	public ConfigHandler(String name, T initialConfig)
+	public ConfigHandler(String name, T initialConfig, Component displayName)
 	{
+		this.name = name;
+		this.displayName = displayName;
 		CONFIG_FILE = Path.of(Platform.getConfigFolder().toString(), "%s.json".formatted(name)).toFile();
 		this.initialConfig = this.config = initialConfig;
-		fields = getAll();
 		load();
 	}
 	
@@ -68,18 +76,24 @@ public class ConfigHandler<T>
 	 *
 	 * @param config The parameter "config" is an object of type T, which is a generic type. The specific type of object being passed as "config" will depend on the context in which this method is being used. The method is designed to save the configuration data of an object to a file using the Gson
 	 */
-	public void save(T config)
+	private void save(T config)
 	{
 		try (FileWriter writer = new FileWriter(CONFIG_FILE))
 		{
+			this.config = config;
 			Gson gson = new GsonBuilder().setPrettyPrinting().disableHtmlEscaping().create();
-			String json = gson.toJson(config);
+			String json = gson.toJson(getAll());
 			writer.write(json);
 			writer.flush();
 		} catch (IOException e)
 		{
 			SimpleModConfig.log.error("Unable to save config file: {}", CONFIG_FILE.getName(), e);
 		}
+	}
+	
+	public void save()
+	{
+		save(config);
 	}
 	
 	/**
@@ -109,33 +123,125 @@ public class ConfigHandler<T>
 	 */
 	public @Nullable Object get(String name)
 	{
+		return get(name, config);
+	}
+	
+	public @Nullable Object getInitial(String name)
+	{
+		return get(name, initialConfig);
+	}
+	
+	private @Nullable Object get(String name, T config)
+	{
 		try
 		{
 			Field field = config.getClass().getDeclaredField(name);
-			return field.get(config);
+			if (hasAnnotation(name))
+			{
+				return field.get(config);
+			}
 		} catch (NoSuchFieldException | IllegalAccessException e)
 		{
 			SimpleModConfig.log.error("Unable to get value of field: {}, from {}", name, CONFIG_FILE.getName(), e);
-			return null;
 		}
+		return null;
 	}
 	
+	private boolean hasAnnotation(String fieldName)
+	{
+		return getConfigOptions(fieldName) != null;
+	}
+	
+	public @Nullable SimpleConfig getConfigOptions(String fieldName)
+	{
+		try
+		{
+			Field field = config.getClass().getField(fieldName);
+			return field.getAnnotation(SimpleConfig.class);
+		} catch (NoSuchFieldException e)
+		{
+			SimpleModConfig.log.error("Unable to get value of field: {}, from {}", fieldName, CONFIG_FILE.getName(), e);
+		}
+		return null;
+	}
+	
+	
+	/**
+	 * Checks if the config exists or not
+	 *
+	 * @param name The config name
+	 * @return true if exists false otherwise
+	 */
 	public boolean exists(String name)
 	{
 		return get(name) != null;
 	}
 	
-	public HashMap<String, Type> getAll()
+	/**
+	 * Returns a HashMap containing all the fields and their corresponding types of the class
+	 * specified in the configuration object.
+	 *
+	 * @return HashMap<String, Type> containing all the fields and their corresponding types
+	 */
+	public HashMap<String, Object> getAll()
 	{
-		if (fields != null)
-			return fields;
-		HashMap<String, Type> map = new HashMap<>();
+		HashMap<String, Object> map = new HashMap<>();
 		Field[] fields = config.getClass().getFields();
 		for (Field field : fields)
 		{
-			map.put(field.getName(), field.getType());
+			try
+			{
+				if (hasAnnotation(field.getName()))
+				{
+					map.put(field.getName(), field.get(config));
+				}
+			} catch (IllegalAccessException ignore)
+			{
+			}
 		}
 		return map;
+	}
+	
+	public LinkedHashMap<String, Object> getAllSorted()
+	{
+		LinkedHashMap<String, Object> sortedMap = new LinkedHashMap<>();
+		getAll().entrySet().stream()
+				.sorted((a, b) -> Integer.compare(getConfigOptions(a.getKey()).index(), getConfigOptions(b.getKey()).index()))
+				.forEachOrdered(entry -> sortedMap.put(entry.getKey(), entry.getValue()));
+		return sortedMap;
+	}
+	
+	public Component getTooltip(String name)
+	{
+		Object value = Objects.requireNonNull(get(name));
+		SimpleConfig options = getConfigOptions(name);
+		assert options != null;
+		MutableComponent tooltip = Component.literal("%s%s%s".formatted(ChatFormatting.AQUA, options.displayName(), ChatFormatting.RESET));
+		if (!options.description().isEmpty())
+		{
+			tooltip.append("\n%s%s%s".formatted(ChatFormatting.GOLD, options.description(), ChatFormatting.RESET));
+		}
+		tooltip.append("\n%sdefault: %s%s%s".formatted(ChatFormatting.GOLD, ChatFormatting.GREEN, get(name, initialConfig), ChatFormatting.RESET));
+		if (options.options().length > 0)
+		{
+			tooltip.append("\n%stype: %sOptions".formatted(ChatFormatting.GOLD, ChatFormatting.GREEN));
+			for (String option : options.options())
+			{
+				tooltip.append("\n%s - %s".formatted(ChatFormatting.BLUE, option));
+			}
+		} else
+		{
+			tooltip.append("\n%stype: %s%s".formatted(ChatFormatting.GOLD, ChatFormatting.GREEN, value.getClass().getSimpleName()));
+		}
+		if (options.min() != Double.MIN_VALUE)
+		{
+			tooltip.append("\n%smin: %s%s".formatted(ChatFormatting.GOLD, ChatFormatting.GREEN, options.min()));
+		}
+		if (options.max() != Double.MAX_VALUE)
+		{
+			tooltip.append("\n%smax: %s%s".formatted(ChatFormatting.GOLD, ChatFormatting.GREEN, options.max()));
+		}
+		return tooltip;
 	}
 	
 	/**
@@ -166,4 +272,57 @@ public class ConfigHandler<T>
 		save(initialConfig);
 	}
 	
+	public void reset(String name)
+	{
+		
+		Field field;
+		try
+		{
+			field = initialConfig.getClass().getDeclaredField(name);
+			@Nullable Object value = field.get(initialConfig);
+			if (value != null)
+			{
+				set(name, value);
+			}
+		} catch (NoSuchFieldException | IllegalAccessException e)
+		{
+			throw new RuntimeException(e);
+		}
+	}
+	
+	/**
+	 * Gets the configs name
+	 *
+	 * @return the config name
+	 */
+	public String getName()
+	{
+		return name;
+	}
+	
+	/**
+	 * Gets the display name
+	 *
+	 * @return the display name component
+	 */
+	public Component getDisplayName()
+	{
+		return displayName;
+	}
+	
+	/**
+	 * Sets the display name
+	 *
+	 * @param displayName the display name component
+	 */
+	public void setDisplayName(Component displayName)
+	{
+		this.displayName = displayName;
+	}
+	
+	@Override
+	public String toString()
+	{
+		return new Gson().toJson(config);
+	}
 }
