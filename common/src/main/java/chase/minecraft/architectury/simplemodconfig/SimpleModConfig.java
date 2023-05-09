effa4d8f3c9862496faa70b5d0b6cb8416f28bba
Drew Chase <dcmanproductions@gmail.com>
2023-05-09 00:02:29 -0400

    added test config

--- common/src/main/java/chase/minecraft/architectury/simplemodconfig/SimpleModConfig.java
+++ common/src/main/java/chase/minecraft/architectury/simplemodconfig/SimpleModConfig.java
@@ -1,5 +1,8 @@
 package chase.minecraft.architectury.simplemodconfig;
 
+import chase.minecraft.architectury.simplemodconfig.handlers.ConfigHandler;
+import chase.minecraft.architectury.simplemodconfig.test.TestConfig;
+import net.minecraft.network.chat.Component;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -7,9 +10,13 @@ public class SimpleModConfig
 {
 	public static final String MOD_ID = "simple-mod-config";
 	public static final Logger log = LogManager.getLogger(MOD_ID);
+	public static SimpleModConfigBuilder builder;
 	
 	public static void init()
 	{
 		log.info("Initializing Simple Mod Config");
+		Component displayName = Component.literal("Test Config");
+		builder = new SimpleModConfigBuilder(new ConfigHandler<>("test-config", new TestConfig(), displayName))
+				.withCommand("test-config", displayName);
 	}
 }
