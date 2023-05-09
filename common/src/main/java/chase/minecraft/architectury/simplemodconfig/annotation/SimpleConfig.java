56c4426bcf8882440901a60063659d40122c9547
Drew Chase <dcmanproductions@gmail.com>
2023-05-09 00:02:02 -0400

    added attribute

---
+++ common/src/main/java/chase/minecraft/architectury/simplemodconfig/annotation/SimpleConfig.java
@@ -0,0 +1,22 @@
+package chase.minecraft.architectury.simplemodconfig.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.FIELD)
+public @interface SimpleConfig
+{
+	int index() default 0;
+	String displayName() default "";
+	
+	String description() default "";
+	
+	double min() default Double.MIN_VALUE;
+	
+	double max() default Double.MAX_VALUE;
+	
+	String[] options() default {};
+}
---
+++ common/src/main/java/chase/minecraft/architectury/simplemodconfig/test/TestConfig.java
@@ -0,0 +1,31 @@
+
+package chase.minecraft.architectury.simplemodconfig.test;
+
+import chase.minecraft.architectury.simplemodconfig.annotation.SimpleConfig;
+
+public class TestConfig
+{
+	@SimpleConfig(index = 1, description = "This is a String template")
+	public String Name = "Hello World";
+	@SimpleConfig(index = 0, description = "This is a option selection template", options = {"Option 1", "Option 2", "Option 3", "Option 4", "Option 5", "Option 6"})
+	public String Options = "Option 1";
+	
+	@SimpleConfig(index = 2)
+	public boolean Toggle = false;
+	
+	@SimpleConfig(index = 3, displayName = "Small Number", description = "This is an example of a 32bit whole number")
+	public int SmallNumber = 5;
+	@SimpleConfig(index = 4, displayName = "Ranged Number", description = "This is an example of a 32bit whole number with a min and max", min = 0, max = 32)
+	public int RangeNumber = 5;
+	
+	@SimpleConfig(index = 5, displayName = "Large Number")
+	public long LargeNumber = 5;
+	
+	@SimpleConfig(index = 6, displayName = "Small Decimal")
+	public float SmallDecimal = .5f;
+	
+	@SimpleConfig(index = 7, displayName = "Large Decimal")
+	public double LargeDecimal = .5d;
+	
+	public String unused = "";
+}
