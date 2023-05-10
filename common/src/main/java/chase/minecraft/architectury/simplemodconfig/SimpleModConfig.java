2c539d34e83b6773e9c897881305271c751ebb0c
Drew Chase <dcmanproductions@gmail.com>
2023-05-09 21:37:16 -0400

    changed mod id

--- common/src/main/java/chase/minecraft/architectury/simplemodconfig/SimpleModConfig.java
+++ common/src/main/java/chase/minecraft/architectury/simplemodconfig/SimpleModConfig.java
@@ -4,12 +4,13 @@ import chase.minecraft.architectury.simplemodconfig.handlers.ConfigHandler;
 import chase.minecraft.architectury.simplemodconfig.test.TestConfig;
 import dev.architectury.platform.Platform;
 import net.minecraft.network.chat.Component;
+import net.minecraft.resources.ResourceLocation;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 public class SimpleModConfig
 {
-	public static final String MOD_ID = "simple-mod-config";
+	public static final String MOD_ID = "simplemodconfig";
 	public static final Logger log = LogManager.getLogger(MOD_ID);
 	public static SimpleModConfigBuilder builder;
 	
@@ -20,8 +21,13 @@ public class SimpleModConfig
 		{
 			Component displayName = Component.literal("Test Config");
 			ConfigHandler<TestConfig> configHandler = new ConfigHandler<>("test-config", new TestConfig(), displayName);
-			builder = new SimpleModConfigBuilder(configHandler)
+			builder = new SimpleModConfigBuilder(configHandler, displayName.getString())
 					.withCommand("test-config", displayName);
 		}
+	}
+	
+	public static ResourceLocation id(String id)
+	{
+		return new ResourceLocation(MOD_ID, id);
 	}
 }
