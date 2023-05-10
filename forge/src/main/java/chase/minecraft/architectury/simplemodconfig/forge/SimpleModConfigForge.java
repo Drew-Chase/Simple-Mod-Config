25319052a6c5a04a117ff1d37aacdb07ae60e46b
Drew Chase <dcmanproductions@gmail.com>
2023-05-09 21:40:01 -0400

    added test dev environment for forge

--- forge/src/main/java/chase/minecraft/architectury/simplemodconfig/forge/SimpleModConfigForge.java
+++ forge/src/main/java/chase/minecraft/architectury/simplemodconfig/forge/SimpleModConfigForge.java
@@ -1,15 +1,29 @@
 package chase.minecraft.architectury.simplemodconfig.forge;
 
-import dev.architectury.platform.forge.EventBuses;
 import chase.minecraft.architectury.simplemodconfig.SimpleModConfig;
+import com.mojang.blaze3d.platform.InputConstants;
+import dev.architectury.platform.Platform;
+import dev.architectury.platform.forge.EventBuses;
+import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.config.ModConfig;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 
 @Mod(SimpleModConfig.MOD_ID)
-public class SimpleModConfigForge {
-    public SimpleModConfigForge() {
+public class SimpleModConfigForge
+{
+	public SimpleModConfigForge()
+	{
 		// Submit our event bus to let architectury register our content on the right time
-        EventBuses.registerModEventBus(SimpleModConfig.MOD_ID, FMLJavaModLoadingContext.get().getModEventBus());
-            SimpleModConfig.init();
-    }
+		EventBuses.registerModEventBus(SimpleModConfig.MOD_ID, FMLJavaModLoadingContext.get().getModEventBus());
+		SimpleModConfig.init();
+  
+  
+		
+		if (Platform.isDevelopmentEnvironment())
+		{
+			SimpleModConfig.builder
+					.withKey(InputConstants.KEY_NUMPAD0, "simplemodconfig.category");
+		}
+	}
 }
