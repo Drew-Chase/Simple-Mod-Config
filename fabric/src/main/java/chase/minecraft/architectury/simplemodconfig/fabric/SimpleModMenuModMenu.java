ce925638d617747fb69266f0a963caf6d97e9555
Drew Chase <dcmanproductions@gmail.com>
2023-05-10 21:17:52 -0400

    moved platform test to common class

--- fabric/src/main/java/chase/minecraft/architectury/simplemodconfig/fabric/SimpleModMenuModMenu.java
+++ fabric/src/main/java/chase/minecraft/architectury/simplemodconfig/fabric/SimpleModMenuModMenu.java
@@ -4,17 +4,12 @@ import chase.minecraft.architectury.simplemodconfig.SimpleModConfig;
 import chase.minecraft.architectury.simplemodconfig.client.gui.screen.ConfigScreen;
 import com.terraformersmc.modmenu.api.ConfigScreenFactory;
 import com.terraformersmc.modmenu.api.ModMenuApi;
-import dev.architectury.platform.Platform;
 
-public class TestModMenu implements ModMenuApi
+public class SimpleModMenuModMenu implements ModMenuApi
 {
 	@Override
 	public ConfigScreenFactory<?> getModConfigScreenFactory()
 	{
-		if (Platform.isDevelopmentEnvironment())
-		{
-			return (ConfigScreenFactory<ConfigScreen>) screen -> new ConfigScreen(SimpleModConfig.builder.configHandler, screen);
-		}
-		return ModMenuApi.super.getModConfigScreenFactory();
+		return (ConfigScreenFactory<ConfigScreen>) screen -> new ConfigScreen(SimpleModConfig.builder.configHandler, screen);
 	}
 }
